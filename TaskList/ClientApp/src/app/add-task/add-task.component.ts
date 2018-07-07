13f2a82d37bdc689b5f8a2fb1426fac80cdbef49
Alexander Solodov <asolodov.me@gmail.com>
2018-07-07 13:53:59 +0300

    add simple validation to add task form

--- TaskList/ClientApp/src/app/add-task/add-task.component.ts
+++ TaskList/ClientApp/src/app/add-task/add-task.component.ts
@@ -1,27 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Task, Status } from '../../models/app.models'
 import { TaskApiService } from '../../services/api/task-api.service';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
 
-  public Task: Task;
+  public taskForm: FormGroup;
 
   constructor(private apiService: TaskApiService) {
-    this.Task = {
-      name: '',
-      dateAdded: null,
-      priority: 0,
-      status: Status.Actvie,
-      timeToComplete: null
+  }
+
+  ngOnInit() {
+    //let ttc = new Date();
+    //ttc.setDate(ttc.getDate() + 1);
+    //let ttcVal = `${ttc.toJSON().slice(0, 10)}T00:00:00`;
+    //let ttcVal = this.getDateTimeFormat(1);
+
+    this.taskForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required
+      ]),
+      description: new FormControl(),
+      priority: new FormControl(1, [
+        Validators.required,
+        Validators.pattern("[1-9]")
+      ]),
+      timeToComplete: new FormControl('', [
+        Validators.required,
+        this.timeToCompleteValidation()
+      ])
+    });
+  }
+
+  timeToCompleteValidation(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const forbidden = new Date(control.value) < new Date();
+      return forbidden ? { 'timeToComplete': { value: control.value } } : null;
     };
   }
 
+  //private getDateTimeFormat(addDays: number): string {
+  //  let ttc = moment().add(1, 'days');
+  //  return ttc.toJSON().slice(0, 19);
+  //}
+
   save() {
-    this.apiService.createTask(this.Task);
+    if (this.taskForm.valid) {
+      this.apiService.createTask(this.taskForm.value);
+    }
   }
 }
--- TaskList/ClientApp/src/app/app.module.ts
+++ TaskList/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -41,6 +41,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FormsModule,
     DxDataGridModule,
+    ReactiveFormsModule,
 
     MatButtonModule,
     MatFormFieldModule,
